293d5bd6d1b77914a6851b23dc89c55e2714f621
Immortalis202 <lmalegori@gmail.com>
2024-03-07 15:49:53 +0100

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,45 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App {
+
+    static int portNumber;
+
+    public static void main(String[] args) throws IOException {
+        int portNumber = Integer.parseInt(args[0]);
+        System.out.println("Server started!");
+        for(;;){
+            ServerSocket serverSocket = null;
+            serverSocket = getServerSocket(portNumber);
+            Socket clientSocket = null;
+            clientSocket = accept(serverSocket);
+
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
+            clientHandler.handle();
+
+        }
+    }
+    private static Socket accept(ServerSocket serverSocket) {
+        Socket clientSocket;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return clientSocket;
+    }
+
+    private static ServerSocket getServerSocket(int portNumber) {
+        ServerSocket serverSocket;
+        try {
+            //noinspection resource
+            serverSocket = new ServerSocket(portNumber);
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return serverSocket;
     }
 }
---
+++ src/main/java/org/example/Ticket.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+public class Ticket {
+	String plate;
+	int slot;
+
+
+	@Override
+	public String toString() {
+		return "Ticket{" +
+				"plate='" + plate + '\'' +
+				", slot=" + slot +
+				'}';
+	}
+}
